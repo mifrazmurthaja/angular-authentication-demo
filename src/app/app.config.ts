810547276d36f5dd8f749643658453ba95436176
mifrazmurthaja <mifrazmurthaja@gmail.com>
2024-12-25 13:32:54 +0530

    update client ID

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,10 +7,8 @@ import { routes } from './app.routes';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://api.asgardeo.io/t/mifrazmurthaja/oauth2/token',
-  //issuer: 'https://uaepass.test:9443/oauth2/token',
   redirectUri: window.location.origin,
   clientId: 'sq4ktPY4lnEDBj8u5kImfHi4nSca',
-  //clientId: 'fEmvZ9EP8nlc8dXl3BHC924xFXUa',
   responseType: 'code',
   scope: 'email openid profile internal_login',
   strictDiscoveryDocumentValidation: false,
